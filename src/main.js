aa8ed1868d34b9f28df30d3867d32042f5896304
Rafael <rafael.arujo1800@gmail.com>
2024-03-05 11:39:48 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const question = document.querySelectorAll('[data-faq-question]');
 
+    const herpsecton = document.querySelectorAll('.hero');
+    const alturahero = herosection.clientheight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoatual = scrollY;
+
+        if (posicaoatual < alturahero) {
+            ocultaelemmentosdoheader();
+        } else {
+            exibeelementosdoheader();
+        }
+    }
+    )
+
+// Secao de atraçao promaçao das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -11,7 +27,30 @@ document.addEventListener('DOMContentLoaded', function () {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+
+// secao FAQ. accordion
+    for (let i =0; i < question.length; i++) {
+        question[i].addEventListener('click', abreOufecharesposta);
+    }
 })
+
+function ocultaelemmentosdoheader () {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeelementosdoheader () {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreOufecharesposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementopai = elemento.target.parentNode;
+
+    elementopai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
